e3dbc34aa7949470630bd20af4354019507c75dc
dmytro <xuluhax@gmail.com>
2022-05-09 23:37:52 +0300

    initial commit

---
+++ hotel-service/src/main/java/com/ws/hotelsearch/config/SpringFoxConfig.java
@@ -0,0 +1,18 @@
+package com.ws.hotelsearch.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringFoxConfig {
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+
+
+
+}
