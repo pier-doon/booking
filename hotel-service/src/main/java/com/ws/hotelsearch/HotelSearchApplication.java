8005bfc09802271bbd1060b508bb42586c9edc97
dmytro <xuluhax@gmail.com>
2022-07-19 14:51:01 +0300

    add Eureka Service

--- hotel-service/src/main/java/com/ws/hotelsearch/HotelSearchApplication.java
+++ hotel-service/src/main/java/com/ws/hotelsearch/HotelSearchApplication.java
@@ -2,10 +2,12 @@ package com.ws.hotelsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEurekaClient
 //@EnableBatchProcessing
 public class HotelSearchApplication {
 
