408eae1952bef349ef2847d0e7d70e88aa8760a9
dmytro <xuluhax@gmail.com>
2022-07-22 18:36:30 +0300

    docs(open-api): add documentation to one endpoint
    Add open-api documentation to addHotel endpoint in HotelController.java

--- hotel-service/src/main/java/com/ws/hotelsearch/controller/HotelController.java
+++ hotel-service/src/main/java/com/ws/hotelsearch/controller/HotelController.java
@@ -5,7 +5,10 @@ import com.ws.hotelsearch.dto.resp.CreateHotelRespDto;
 import com.ws.hotelsearch.dto.resp.SearchFetchHotelRespDto;
 import com.ws.hotelsearch.exception.EmptyPhotoRequestException;
 import com.ws.hotelsearch.service.HotelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +21,22 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/hotel")
 @RequiredArgsConstructor
-@ApiResponse(description = "Api for work with hotels")
 public class HotelController {
 
     private final HotelService hotelService;
 
+    @Operation(summary = "Create hotel in db without uploading photo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Added hotel in db", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Not available", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<CreateHotelRespDto> addHotel(@RequestBody CreateHotelReqDto reqDto) {
         CreateHotelRespDto hotelResponse = hotelService.addHotel(reqDto);
         return ResponseEntity.ok().body(hotelResponse);
     }
-//TODO return photos ids that recently added
-    @PutMapping(value = "{id}/image/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+
+    @PutMapping(value = "/{id}/image/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<List<String>> addPhotosToHotel(@PathVariable UUID id, @RequestPart List<MultipartFile> photos) {
         if (!photos.isEmpty()) {
             List<String> photoNames = hotelService.addPhotos(id, photos);
@@ -39,13 +46,15 @@ public class HotelController {
         }
     }
 
-    @PutMapping("{id}/image/cover/{name}")
+    @PutMapping("/{id}/image/cover/{name}")
     public ResponseEntity<String> setCoverForHotel(@PathVariable UUID id, @PathVariable String name) {
         hotelService.setCover(id, name);
         return ResponseEntity.ok().build();
     }
+
     @GetMapping
     public ResponseEntity<List<SearchFetchHotelRespDto>> getAllHotels() {
+        System.out.println("oioiio");
         List<SearchFetchHotelRespDto> hotels = hotelService.getAllHotels();
         return ResponseEntity.ok().body(hotels);
     }
